28bc989fc8285d6c3e70a14cc9ab758c9802f087
einsteinsci <einsteinsci@gmail.com>
2015-08-15 23:41:29 -0700

    Starting NEI support for MC1.8

---
+++ src/main/java/net/einsteinsci/betterbeginnings/nei/NEIConfig.java
@@ -0,0 +1,38 @@
+package net.einsteinsci.betterbeginnings.nei;
+
+import codechicken.nei.api.API;
+import codechicken.nei.api.IConfigureNEI;
+import net.einsteinsci.betterbeginnings.ModMain;
+import net.einsteinsci.betterbeginnings.register.RegisterBlocks;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Optional;
+
+// NEI support wizardry
+@Optional.Interface(iface = "codechicken.nei.api.IConfigureNEI", modid = "NotEnoughItems")
+public class NEIConfig implements IConfigureNEI
+{
+	@Override
+	public void loadConfig()
+	{
+		API.hideItem(new ItemStack(RegisterBlocks.kilnLit));
+		API.hideItem(new ItemStack(RegisterBlocks.brickOvenLit));
+		API.hideItem(new ItemStack(RegisterBlocks.smelterLit));
+		API.hideItem(new ItemStack(RegisterBlocks.obsidianKilnLit));
+		API.hideItem(new ItemStack(RegisterBlocks.netherBrickOvenLit));
+		API.hideItem(new ItemStack(RegisterBlocks.enderSmelterLit));
+
+		API.hideItem(new ItemStack(RegisterBlocks.campfireLit));
+	}
+
+	@Override
+	public String getName()
+	{
+		return ModMain.NAME;
+	}
+
+	@Override
+	public String getVersion()
+	{
+		return ModMain.VERSION;
+	}
+}
