70fc7faa05f1cd2ecf3f48120722cee648688802
Dylan Knox <knoxdk@uwec.edu>
2015-12-31 15:13:21 -0600

    Fixes #99, tags sync custom names

--- src/main/java/net/einsteinsci/betterbeginnings/tileentity/TileEntityEnderSmelter.java
+++ src/main/java/net/einsteinsci/betterbeginnings/tileentity/TileEntityEnderSmelter.java
@@ -14,6 +14,9 @@ import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.server.gui.IUpdatePlayerListBox;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.*;
@@ -538,7 +541,19 @@ public class TileEntityEnderSmelter extends TileEntity implements IUpdatePlayerL
 		return ModMain.MODID + ":enderSmelter";
 	}
 
-
+	@Override
+	public Packet getDescriptionPacket()
+	{
+		NBTTagCompound tag = new NBTTagCompound();
+		writeToNBT(tag);
+		return new S35PacketUpdateTileEntity(pos, 1, tag);
+	}
+	
+	@Override
+	public void onDataPacket(NetworkManager manager, S35PacketUpdateTileEntity packet)
+	{
+		readFromNBT(packet.getNbtCompound());
+	}
 
 
 
--- src/main/java/net/einsteinsci/betterbeginnings/tileentity/TileEntityKiln.java
+++ src/main/java/net/einsteinsci/betterbeginnings/tileentity/TileEntityKiln.java
@@ -16,6 +16,9 @@ import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.server.gui.IUpdatePlayerListBox;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.*;
@@ -525,4 +528,18 @@ public class TileEntityKiln extends TileEntity implements IUpdatePlayerListBox, 
 	}
 
 
+	@Override
+	public Packet getDescriptionPacket()
+	{
+		NBTTagCompound tag = new NBTTagCompound();
+		writeToNBT(tag);
+		return new S35PacketUpdateTileEntity(pos, 1, tag);
+	}
+	
+	@Override
+	public void onDataPacket(NetworkManager manager, S35PacketUpdateTileEntity packet)
+	{
+		readFromNBT(packet.getNbtCompound());
+	}
+	
 }
